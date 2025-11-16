b43b1ddd0b176dc42a68c30f208168efa6cfafa9
shimul705 <freelancershimul172@gmail.com>
2025-11-16 22:21:53 +0600

    implimented typeof number and boolean functionality

--- solution.ts
+++ solution.ts
@@ -3,15 +3,14 @@
 function formatValue(value: string | number | boolean): string | number | boolean {
     if (typeof value === 'string') {
         return value.toUpperCase();
+    } else if (typeof value === 'number') {
+        return value * 10;
+    } else if (typeof value === 'boolean') {
+        return !value;
     }
 
 }
 
-
 console.log(formatValue('hello'));
-
-
-
-
-
-
+console.log(formatValue(5));
+console.log(formatValue(true));
