f11919fe280d86e68860702c552a18677ef1fefe
shimul705 <freelancershimul172@gmail.com>
2025-11-17 00:22:31 +0600

    P-3 Removed console log and fixed formatting for better view attempt 2

--- solution.ts
+++ solution.ts
@@ -40,8 +40,3 @@ class Person {
 }
 
 
-const person1 = new Person('John Doe', 30);
-console.log(person1.getDetails());
-
-const person2 = new Person('Alice', 25);
-console.log(person2.getDetails());
