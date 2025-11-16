3941b780a0371f99f0caa0e529d8062856d8cbf4
shimul705 <freelancershimul172@gmail.com>
2025-11-16 22:14:19 +0600

    implimented string type retuns uppercase

--- solution.ts
+++ solution.ts
@@ -1,8 +1,17 @@
 
 
 function formatValue(value: string | number | boolean): string | number | boolean {
-
+    if (typeof value === 'string') {
+        return value.toUpperCase();
+    }
 
 }
+
+
+console.log(formatValue('hello'));
+
+
+
+
 
 
