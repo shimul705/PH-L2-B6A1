a0b806dbb0ee80d6aa25a7998828c4c43d80e72e
shimul705 <freelancershimul172@gmail.com>
2025-11-16 22:41:59 +0600

    implimented if it's array then return length of array

--- solution.ts
+++ solution.ts
@@ -16,11 +16,14 @@ function formatValue(value: string | number | boolean): string | number | boolea
 function getLength(value: string | any[]): number {
     if (typeof value === 'string') {
         return value.length;
+    } else if (Array.isArray(value)) {
+        return value.length;
     }
 
     return 0;
 }
 
 console.log(getLength('typescript'));
+console.log(getLength([10, 20, 30, 40]));
 
 
