cf4f41952ea4cdd41398dbf7a95603415529cdc6
shimul705 <freelancershimul172@gmail.com>
2025-11-17 15:46:28 +0600

    P-7 created interface for book

--- solution.ts
+++ solution.ts
@@ -106,11 +106,16 @@ const filterActiveUsers: FilterActiveUsers = (users) => {
 };
 
 
-const users = [
-    { id: 1, name: 'Rakib', email: 'rakib@example.com', isActive: true },
-    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-    { id: 2, name: 'Asha', email: 'asha@example.com', isActive: false },
-    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-    { id: 3, name: 'Rumi', email: 'rumi@example.com', isActive: true },
-];
-console.log(filterActiveUsers(users));
+
+
+interface Book {
+    title: string;
+    author: string;
+    publishedYear: number;
+    isAvailable: boolean;
+}
+
+
+
+
+
